9d1b6484581f92e16c0dcff41e7a1d104f4159d4
mentuyu0001 <119647679+mentuyu0001@users.noreply.github.com>
2025-11-05 16:51:14 +0900

    fix(to-title):to-titleを修正

--- Assets/Scripts/System/UI/ToTitle.cs
+++ Assets/Scripts/System/UI/ToTitle.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class ToTitle : MonoBehaviour
 {
     [SerializeField] private GameSceneManager gameSceneManager;
+    [SerializeField] private FadeController fadecontroller;
 
-    
+    public void OnClick()
+    {
+        
+    }
 }
